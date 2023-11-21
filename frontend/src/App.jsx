039ec0a56690411e86600d00e3ecdf4c8c1dcd76
tayfurparmak <tayfur-parmak-1907@hotmail.com>
2023-11-21 17:39:37 +0300

    FİRST COMMİT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/header';
+import './App.css'
+import Footer from './components/footer';
+import Policy from './components/policy';
+
+function App() {
+ 
+
+  return (
+   <div>
+    <Header/>
+      <h1>dasd</h1>
+      <Policy/>
+      <Footer/>
+   </div>
+     
+  );
+}
+
+export default App
