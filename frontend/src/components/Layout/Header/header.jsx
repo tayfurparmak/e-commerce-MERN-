c7e6179119fe9e279d90f289d2db1be8b094b374
tayfurparmak <tayfur-parmak-1907@hotmail.com>
2023-11-22 16:42:13 +0300

    componentDynmic

--- frontend/src/components/Layout/Header/header.jsx
+++ frontend/src/components/Layout/Header/header.jsx
@@ -1,6 +1,6 @@
 import "./Header.css";
-
-const Header = () => {
+import Proptypes from "prop-types";
+const Header = ({ setIsSearchShow }) => {
   return (
     <header>
       <div className="global-notification">
@@ -189,7 +189,8 @@ const Header = () => {
                 <a href="account.html" className="header-account">
                   <i className="bi bi-person"></i>
                 </a>
-                <button className="search-button">
+                <button className="search-button"
+                onClick={() => setIsSearchShow(true)}>
                   <i className="bi bi-search"></i>
                 </button>
                 <a href="#">
@@ -211,3 +212,7 @@ const Header = () => {
 };
 
 export default Header;
+
+Header.propTypes = {
+  setIsSearchShow: Proptypes.func,
+};
--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Footer from "../components/Layout/Footer/footer";
 import Header from "../components/Layout/Header/header";
 import Proptypes from "prop-types";
+import Search from "../components/Modals/Search/Search";
+import { useState } from "react";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
-const MainLayout = ({ children }) => {
+    const MainLayout = ({ children }) => {
+        const [isSearchShow, setIsSearchShow] = useState(false);
+        const [isDialogShow, setIsDialogShow] = useState(true);
   return (
-    <React.Fragment>
-      <Header />
+    <div className="main-layout">
+           <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
+            <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+      <Header setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
-    </React.Fragment>
+    </div>
   );
 };
 
